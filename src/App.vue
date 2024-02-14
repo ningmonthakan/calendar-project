a1b5d018dde5b6105c90444dc9394f78b3639e31
ningmonthakan <159982480+ningmonthakan@users.noreply.github.com>
2024-02-14 11:29:08 +0700

    งาน

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Calendar 
+    render-style="background" 
+    :min-date="new Date()"
+    ></Calendar>
+  </div>
+</template>
+
+<script>
+import Calendar from 'v-year-calendar';
+export default {
+  name: 'App',
+  components: {
+    Calendar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
